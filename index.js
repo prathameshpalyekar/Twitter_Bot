a4825e802995d52bb08780fc935112e614e9f257
Prathamesh Palyekar <prathameshpalyekar@Prathameshs-MacBook-Pro.local>
2017-05-18 23:36:22 +0530

    Getting followers data

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var Twit = require('twit');
 var bot = new Twit({
-	consumer_key: process.env.LEARNING_BOT_CONSUMER_KEY,
-	consumer_secret: process.env.LEARNING_BOT_CONSUMER_SECRET,
-	access_token: process.env.LEARNING_BOT_ACCESS_TOKEN,
-	access_token_secret: process.env.LEARNING_BOT_ACCESS_TOKEN_SECRET,
-	timeout_ms: 60 * 1000
+    consumer_key: process.env.LEARNING_BOT_CONSUMER_KEY,
+    consumer_secret: process.env.LEARNING_BOT_CONSUMER_SECRET,
+    access_token: process.env.LEARNING_BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.LEARNING_BOT_ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000
 });
 
-bot.post('statuses/update',{status: 'Hello world !'}
-, function(err, data, response){
-	if(err) {
-		console.log(err);
-	} else {
-		console.log(data.text + ' was tweeted.');
-	}
-})
+// bot.post('statuses/update',{status: 'Hello world !'}
+// , function(err, data, response){
+// 	if(err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(data.text + ' was tweeted.');
+// 	}
+// })
+
+bot.get('followers/ids', {screen_name: 'Pratham_bot'}, 
+    function(err, data, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data)
+        }
+    }
+)
